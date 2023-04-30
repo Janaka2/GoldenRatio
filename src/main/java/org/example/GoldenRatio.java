c9756498950e3fad43c6698c170c6039923dc3a5
Janaka Premathilaka <janaka2@gmail.com>
2023-04-30 10:52:57 +0200

    Initial commit

--- src/main/java/org/example/GoldenRatio.java
+++ src/main/java/org/example/GoldenRatio.java
@@ -1,7 +1,23 @@
 package org.example;
 
-public class Main {
+public class GoldenRatio {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int n = 100; // number of iterations for Fibonacci sequence
+        double goldenRatio = calculateGoldenRatio(n);
+        System.out.println("Golden Ratio: " + goldenRatio);
+    }
+
+    public static double calculateGoldenRatio(int n) {
+        double[] fibonacciNumbers = new double[n];
+
+        fibonacciNumbers[0] = 0;
+        fibonacciNumbers[1] = 1;
+
+        for (int i = 2; i < n; i++) {
+            fibonacciNumbers[i] = fibonacciNumbers[i - 1] + fibonacciNumbers[i - 2];
+        }
+
+        return fibonacciNumbers[n - 1] / fibonacciNumbers[n - 2];
     }
 }
